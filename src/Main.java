68ceb4fafc4ca89cee3c8b74b7b4f14f93752362
iShantaram <1agapovav987@gmail.com>
2022-10-14 04:54:18 +0300

    Homework7 chapter4.1 arrays completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,74 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // HW7 Arrays1
+
+        // HW1
+        // Task1
+        System.out.println("\nTask 1:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        // Task2
+        System.out.println("\nTask 2:");
+        for (int i = 10; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        // Task3
+        System.out.println("\nTask 3:");
+        for (int i = 0; i <= 17; i+=2) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        // Task4
+        System.out.println("\nTask 4:");
+        for (int i = 10; i >= -10; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        // HW2
+        // Task1
+        System.out.println("\nTask 1:");
+        for (int i = 1904; i <= 2096; i+=4) {
+            System.out.println(i + " год является високосным");
+        }
+
+        // Task2
+        System.out.println("\nTask 2:");
+        for (int i = 7; i <= 98; i+=7) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        // Task3
+        System.out.println("\nTask 3:");
+        for (int i = 1; i <= 512; i*=2) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        // HW3
+        // Task1
+        System.out.println("\nTask 1:");
+        double vklad = 29000;
+        double vkladSummary = 0;
+        for (int i = 1; i <= 12; i++) {
+            vkladSummary += vklad;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + vkladSummary + " рублей");
+        }
+
+        // Task2
+        System.out.println("\nTask 2:");
+        vkladSummary = 0;
+        for (int i = 1; i <= 12; i++) {
+            vkladSummary = vkladSummary * 1.01 + vklad;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + vkladSummary + " рублей");
+        }
+
     }
 }
